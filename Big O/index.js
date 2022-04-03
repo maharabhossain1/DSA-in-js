2e446f215c93f7dbf2441c2e67e5a75eb2f7c024
maharabhossain1 <maharabhossain0022@gmail.com>
2022-04-04 02:00:35 +0600

    space complexity

--- Big O/index.js
+++ Big O/index.js
@@ -28,6 +28,7 @@ console.log(`Time is ${(t2 - t1) / 1000} sec`);
 ///////////////////////////////////////////////////////////////////////////
 // ------------------------------ (((((((x))))))) -----------------------//
 ///////////////////////////////////////////////////////////////////////////
+// Time Complexity
 // Example -01
 function countUpandDown(n) {
   console.log("going Up");
@@ -55,3 +56,44 @@ function nestedLoop(n) {
   }
 }
 // In this Example Big O is  O(n^2)
+///////////////////////////////////////////////////////////////////////////
+// ------------------------------ (((((((x))))))) -----------------------//
+///////////////////////////////////////////////////////////////////////////
+// Space Complexity or Auxiliry Space Complexity
+function logUpTo(n) {
+  for (var i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+// Big o in O(1) space
+//
+function logAtMost10(n) {
+  for (var i = 1; i <= Math.min(n, 10); i++) {
+    console.log(i);
+  }
+}
+// Big o in O(1) space
+//
+function onlyElementsAtEvenIndex(array) {
+  var newArray = Array(Math.ceil(array.length / 2));
+  for (var i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      newArray[i / 2] = array[i];
+    }
+  }
+  return newArray;
+}
+// Big o in O(n) space
+//
+function subtotals(array) {
+  var subtotalArray = Array(array.length);
+  for (var i = 0; i < array.length; i++) {
+    var subtotal = 0;
+    for (var j = 0; j <= i; j++) {
+      subtotal += array[j];
+    }
+    subtotalArray[i] = subtotal;
+  }
+  return subtotalArray;
+}
+// Big o in O(n) space
