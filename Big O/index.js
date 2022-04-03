82626fe95987c0bfe6de8938b995e64e089644e3
maharabhossain1 <maharabhossain0022@gmail.com>
2022-04-04 01:31:41 +0600

    example -1

--- Big O/index.js
+++ Big O/index.js
@@ -20,6 +20,27 @@ console.log(add2(6));
 /// Time manage
 // For number 01
 let t1 = performance.now();
-add(1000000000);
+// add(1000000000);
+add2(1000000000000000000); // This is the best solutions
+
 let t2 = performance.now();
 console.log(`Time is ${(t2 - t1) / 1000} sec`);
+///////////////////////////////////////////////////////////////////////////
+// ------------------------------ (((((((x))))))) -----------------------//
+///////////////////////////////////////////////////////////////////////////
+// Example -01
+function countUpandDown(n) {
+  console.log("going Up");
+  // for this loop Big O in o(n)
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("Going Down ");
+  //Again for this  one is also loop Big O in o(n)
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("all done ");
+}
+// In total
+// In this Example Big O is only N  that means o(n)
