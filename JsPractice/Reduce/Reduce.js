63d722c1aa47e4782a41e7c0a40f8106d3286279
maharabhossain1 <maharabhossain0022@gmail.com>
2022-06-08 22:55:23 +0600

    Reduce Practice2

--- JsPractice/Reduce/Reduce.js
+++ JsPractice/Reduce/Reduce.js
@@ -17,3 +17,39 @@ function common(...arr) {
 const commonValue = common([1, 4, 6, 5, 3], [4, 3, 6, 9], [9, 7, 4, 6, 2]);
 
 console.log("Common", commonValue);
+
+//
+//
+const products = [
+  {
+    productId: 234435,
+    colorId: 1,
+    size: "11-M",
+  },
+  {
+    productId: 443434,
+    colorId: 2,
+    size: "11-M",
+  },
+  {
+    productId: 343434,
+    colorId: 1,
+    size: "11-L",
+  },
+  {
+    productId: 322333,
+    colorId: 2,
+    size: "12-M",
+  },
+];
+
+function fieldByIndex(products, field) {
+  return products.reduce((acc, curr) => {
+    const key = curr[field];
+    const value = acc[key] ? [...acc[key], curr] : [curr];
+    acc[key] = value;
+    return acc;
+  }, {});
+}
+
+console.log(fieldByIndex(products, "size"));
